8a1e0e78d42656f3f42107d79265b5e3a634ddba
anon <>
2023-10-27 23:21:38 -0700

    sync

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
 import setuptools
+import pathlib
 
+this_directory = pathlib.Path(__file__).parent
 
 setuptools.setup(
     name='scrapbook-ext',
     version='0.0.0a',
     description='Extensions for scrapbook',
+    long_description=(this_directory / 'README.md').read_text(),
+    long_description_content_type='text/markdown',
     python_requires='>=3.6',
-    packages=['scrapbook_ext'],
     package_dir={
         '': 'src'
     },
+    packages=['scrapbook_ext'],
     install_requires=[
         'scrapbook'
     ],

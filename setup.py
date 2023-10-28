a1add81fd2032474394d0a1f63bbf30ba6fba58b
anon <>
2023-10-28 01:31:49 -0700

    sync

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ this_directory = pathlib.Path(__file__).parent
 
 setuptools.setup(
     name='scrapbook-ext',
-    version='0.0.0a1',
+    version='0.0.0a2',
     description='Extensions for scrapbook',
     long_description=(this_directory / 'README.md').read_text(),
     long_description_content_type='text/markdown',
     python_requires='>=3.6',
     package_dir={
-        '': 'src'
+        '': 'packages'
     },
-    packages=['scrapbook_ext'],
+    packages=setuptools.find_packages(where='packages'),
     install_requires=[
         'scrapbook'
     ],
